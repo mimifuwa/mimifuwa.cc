d89da671559ceaf9bba8aac8daf0a69da6e972a0
mimifuwa <mimifuwa.cc@gmail.com>
2025-03-25 01:34:33 +0900

    chore: eslint&prettierを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+// eslint.config.js
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,45 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    ignores: ["node_modules/", "dist/", "build/", ".next/", "**/*.min.js"],
+  },
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "sort-imports": [
+        "error",
+        { ignoreCase: true, ignoreDeclarationSort: true },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["sibling", "parent"],
+            "object",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
